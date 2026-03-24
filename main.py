991a5cb672a7c5150385b3182716facbfe676159
vivek <vivek.athilkar100@gmail.com>
2026-03-23 23:48:52 -0700

    testing
    testing

--- main.py
+++ main.py
@@ -10,7 +10,7 @@ WAQI_TOKEN = os.environ.get("WAQI_TOKEN")
 CITY = os.environ.get("CITY")
 
 publisher = pubsub_v1.PublisherClient()
-topic_path = publisher.topic_path(PROJECT_ID, TOPIC_ID)
+topic_path = f"projects/{PROJECT_ID}/topics/{TOPIC_ID}"
 
 def fetch_waqi_data(request):
     url = f"https://api.waqi.info/feed/{CITY}/?token={WAQI_TOKEN}"
@@ -34,6 +34,10 @@ def fetch_waqi_data(request):
 
     return "Published", 200
 
+# Run the below command in cloud shell to check if the cloud function is working fine.
+"""
+curl -X GET https://asia-south1-project-5dd8f491-cc9c-4f1e-951.cloudfunctions.net/fetch_waqi_data
+"""
 
 """
 gcloud projects add-iam-policy-binding project-5dd8f491-cc9c-4f1e-951 \
@@ -69,4 +73,24 @@ gcloud projects add-iam-policy-binding project-5dd8f491-cc9c-4f1e-951 \
   --member="serviceAccount:306164924329@cloudbuild.gserviceaccount.com" \
   --role="roles/artifactregistry.reader"
 
+"""
+
+# Cloud Scheduler
+"""
+gcloud scheduler jobs create http waqi-hourly-job \
+  --location asia-south1 \
+  --schedule "10 * * * *" \
+  --uri "https://asia-south1-project-5dd8f491-cc9c-4f1e-951.cloudfunctions.net/fetch_waqi_data" \
+  --http-method GET \
+  --time-zone "Asia/Kolkata"
+"""
+
+# Check scheduler jobs
+"""
+gcloud scheduler jobs list --location asia-south1
+"""
+
+# Update schedule
+"""
+gcloud scheduler jobs update http waqi-hourly-job --location asia-south1 --schedule "10 12 * * *"
 """
