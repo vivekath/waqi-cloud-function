177a4630eb2f7b0577c7a53902d8a683436c49c1
vivek <vivek.athilkar100@gmail.com>
2026-03-23 22:39:46 -0700

    testing
    testing

--- main.py
+++ main.py
@@ -34,34 +34,39 @@ def fetch_waqi_data(request):
 
     return "Published", 200
 
-"""
-PROJECT_ID="project-5dd8f491-cc9c-4f1e-951"
-PROJECT_NUMBER=$(gcloud projects describe $PROJECT_ID --format="value(projectNumber)")
-CLOUDBUILD_SA="$PROJECT_NUMBER@cloudbuild.gserviceaccount.com"
 
-# Cloud Functions Developer
-gcloud projects add-iam-policy-binding $PROJECT_ID \
-  --member="serviceAccount:$CLOUDBUILD_SA" \
+"""
+gcloud projects add-iam-policy-binding project-5dd8f491-cc9c-4f1e-951 \
+  --member="serviceAccount:306164924329-compute@developer.gserviceaccount.com" \
   --role="roles/cloudfunctions.developer"
 
-# Service Account User
-gcloud projects add-iam-policy-binding $PROJECT_ID \
-  --member="serviceAccount:$CLOUDBUILD_SA" \
+  
+gcloud projects add-iam-policy-binding project-5dd8f491-cc9c-4f1e-951 \
+  --member="serviceAccount:306164924329-compute@developer.gserviceaccount.com" \
   --role="roles/iam.serviceAccountUser"
 
-# For Gen 2 functions, also needed:
-gcloud projects add-iam-policy-binding $PROJECT_ID \
-  --member="serviceAccount:$CLOUDBUILD_SA" \
-  --role="roles/run.admin"
+  
+gcloud projects add-iam-policy-binding project-5dd8f491-cc9c-4f1e-951 \
+  --member="serviceAccount:306164924329-compute@developer.gserviceaccount.com" \
+  --role="roles/artifactregistry.writer"
 
-gcloud projects add-iam-policy-binding $PROJECT_ID \
-  --member="serviceAccount:$CLOUDBUILD_SA" \
-  --role="roles/eventarc.admin"
 
-# If publishing to Pub/Sub:
-gcloud projects add-iam-policy-binding $PROJECT_ID \
-  --member="serviceAccount:$CLOUDBUILD_SA" \
-  --role="roles/pubsub.publisher"
+# Compute service account
+gcloud projects add-iam-policy-binding project-5dd8f491-cc9c-4f1e-951 \
+  --member="serviceAccount:306164924329-compute@developer.gserviceaccount.com" \
+  --role="roles/artifactregistry.writer"
 
+gcloud projects add-iam-policy-binding project-5dd8f491-cc9c-4f1e-951 \
+  --member="serviceAccount:306164924329-compute@developer.gserviceaccount.com" \
+  --role="roles/artifactregistry.reader"
+
+# Cloud Build service account
+gcloud projects add-iam-policy-binding project-5dd8f491-cc9c-4f1e-951 \
+  --member="serviceAccount:306164924329@cloudbuild.gserviceaccount.com" \
+  --role="roles/artifactregistry.writer"
+
+gcloud projects add-iam-policy-binding project-5dd8f491-cc9c-4f1e-951 \
+  --member="serviceAccount:306164924329@cloudbuild.gserviceaccount.com" \
+  --role="roles/artifactregistry.reader"
 
 """
